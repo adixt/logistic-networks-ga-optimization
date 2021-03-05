b885bf2b029de8a9ceb9ec82c3ed1de1323277f2
Adam Dziomdziora <190140@edu.p.lodz.pl>
2021-03-05 12:56:00 +0100

    parafor loops

--- CostFunction.m
+++ CostFunction.m
@@ -2,16 +2,17 @@ function Population = CostFunction(a, start_xd_min, individuals)
 
     popsize = length(individuals);
     % COST FUNCTION
-    for i = 1:popsize
-        a = simulate(a, start_xd_min, individuals(:, :, i));
-        tmp.cost = a.fitness;
-        tmp.chrom = a.LA_nom;
-        tmp.BE = a.w4;
+    parfor i = 1:popsize
+        result = simulate(a, start_xd_min, individuals(:, :, i));
+        tmp = [];
+        tmp.cost = result.fitness;
+        tmp.chrom = result.LA_nom;
+        tmp.BE = result.w4;
         Population(i) = tmp;
     end
 
     % Sort the population members from best to worst
-    Cost = zeros(1, popsize);
+    Cost = zeros(1, popsize, 'gpuArray');
 
     for i = 1:popsize
         Cost(i) = Population(i).cost;
